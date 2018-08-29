9e1f698214cc213e187d9bfc11b608fb2f6c0977
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-29 00:13:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
   //write fetch request to the Game of Thrones API
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
